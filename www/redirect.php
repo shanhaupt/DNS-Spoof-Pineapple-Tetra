716994a7f2a9abd123fb17f31a646e7dfdf5d69a
Shan Haupt <shanhaupt@gmail.com>
2020-03-27 12:09:02 -0700

    Update redirect.php

--- www/redirect.php
+++ www/redirect.php
@@ -1,6 +1,5 @@
 <?php
 $ref = $_SERVER['HTTP_HOST'];
 if ($ref == "www.school.com")   { header('Location: school.html'); }
-if ($ref == "www.pornhub.com")  { header('Location: pornhub.html'); }
 require('peets.html');
 ?>
